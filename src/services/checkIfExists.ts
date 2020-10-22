b8a2577e9fd4957c78536ec10b49ab1e8fe37ca3
EmersonBraun <emersonfbraun@gmail.com>
2020-10-22 18:05:49 -0300

    feat: add services

---
+++ src/services/checkIfExists.ts
@@ -0,0 +1,5 @@
+import { post } from '../libs/api'
+
+export async function checkIfExists(proc: string, params: object, debug = false) {
+  return await post(`${proc}/portarias`, params, false, false, debug)
+}
