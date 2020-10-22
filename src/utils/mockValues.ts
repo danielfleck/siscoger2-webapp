02ab5332db6b2743a2849eb5f4605d4ebdb105f2
EmersonBraun <emersonfbraun@gmail.com>
2020-10-22 18:07:00 -0300

    feat: add utils

---
+++ src/utils/mockValues.ts
@@ -0,0 +1,12 @@
+export const randomIntFromInterval = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1) + min)
+export const getCurrentDate = (extensive = false) => {
+  const date = new Date();
+  const options = { year: "numeric", month: "long", day: "numeric" };
+
+  if (extensive) {
+    return date.toLocaleDateString("pt-br", options)
+  }
+
+  return date.toLocaleDateString("pt-br", { ...options, month: 'numeric'})
+
+}
